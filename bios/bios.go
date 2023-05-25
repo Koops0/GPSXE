f1f029ddd9247d8ccf65000b99bfacbbfa1a2ec0
Koops0 <kersh3711@gmail.com>
2023-05-25 10:55:21 -0400

    Updated BIOS, added Interconnect

---
+++ bios/bios.go
@@ -0,0 +1,45 @@
+package bios
+
+import (
+	"os"
+)
+
+const BIOS_SIZE uint64 = 512 * 1024
+
+type BIOS struct {
+	data []uint8
+}
+
+func (b *BIOS) New(path string) (*BIOS, error) {
+	file, err := os.Open("SCPH1001.bin")
+
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	b.data = make([]uint8, 0)
+
+	_, err = file.Read(b.data)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(b.data) == int(BIOS_SIZE) {
+		return &BIOS{data: b.data}, nil
+	} else {
+		return nil, err
+	}
+}
+
+func (b *BIOS) load32(offset uint32) uint32 {
+	b0 := uint32(b.data[offset])
+	b1 := uint32(b.data[offset+1])
+	b2 := uint32(b.data[offset+2])
+	b3 := uint32(b.data[offset+3])
+
+	res := b0 | (b1 << 8) | (b2 << 16) | (b3 << 24)
+
+	return 1
+}
---
+++ interconnect.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"./bios"
+)
+
+type Interconnect struct {
+	bios bios.BIOS
+}
+
+func (i Interconnect) new (bios bios.BIOS) Interconnect{
+	
+}
